b38970fb5319a1c229d42f14de5552f25e21eb95
williamycsiu <wsiu4@hawaii.edu>
2016-10-24 21:29:53 -1000

    Revert "digit template as base template"
    This reverts commit 14d46bef46f153a19b02655f0495c9fb4092765d.

--- app/imports/startup/server/publications.js
+++
@@ -1,6 +0,0 @@
-import { Contacts } from '../../api/contacts/contacts.js';
-import { Meteor } from 'meteor/meteor';
-
-Meteor.publish('Contacts', function publishContactsData() {
-  return Contacts.find();
-});
